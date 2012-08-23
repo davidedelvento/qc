a0926d400f361dd23e36fa11db9317947c18d73e
Davide Del Vento <davide.del.vento@gmail>
2012-08-23 11:23:44 -0600

    first unittest example

---
+++ example1_unittest.py
@@ -0,0 +1,30 @@
+from unittest import TestCase, main
+from qc import forall, unicodes
+
+# This example is adapted from Scala's
+# https://github.com/rickynils/scalacheck
+# and we are pretending to test the string
+# concatenation, slicing and the len builtin
+
+class TestString(TestCase):
+    @forall(tries=2000, a=unicodes(), b=unicodes())
+    def testStartswith(self, a, b):
+        concat = a + b
+        self.assertTrue(concat.startswith(a))
+
+    @forall(tries=10, a=unicodes(), b=unicodes())
+    def testConcatenation(self, a, b):
+        concat = a + b
+        # the following is meant to fail as an example of a failure
+        self.assertTrue(len(concat) > len(a))
+        self.assertTrue(len(concat) > len(b))
+
+    @forall(a=unicodes(), b=unicodes(), c=unicodes())
+    def testSubstring(self, a, b, c):
+        concat = a + b + c
+        start = len(a)
+        stop = len(a) + len(b)
+        self.assertEqual(concat[start: stop], b)
+
+main() 
+
